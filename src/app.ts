cbcbd87b2f6b3d9c69a2549b5d7fd68d8bce2291
pedroesmerio <p.h.esmerio@gmail.com>
2021-10-21 01:38:28 -0300

    Criar rota login github

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import "dotenv/config";
 
+import { router } from "./routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
 
 app.listen(4000, () => console.log(`Server is running on PORT :4000`));
