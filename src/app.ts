42d8a11dd47553e66a520dcd8f25bc6c4714efc1
pedroesmerio <p.h.esmerio@gmail.com>
2021-10-21 01:40:32 -0300

    Criar rota callback

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,9 @@ app.get('/github', (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
 });
 
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+})
+
 app.listen(4000, () => console.log(`Server is running on PORT :4000`));
